9f84420b7b29f4f0eff0aa5fa92ed425ad585c8d
Andrew Euredjian <andrew_103@rocketmail.com>
2020-11-03 21:53:50 +0000

    make small changes mentioned in #1

--- trajectory_generator/generator.py
+++ trajectory_generator/generator.py
@@ -7,11 +7,18 @@ class TrajectoryGenerator(Node):
 
   def __init__(self):
     super().__init__('trajectory_generator')
-    self._publisher = self.create_publisher(Trajectory, 'new_traj', 10)
-    self._subscription = self.create_subscription(SensorData, 'traj_start_data', self.create_trajectory_callback, 10)
+    self._publisher = self.create_publisher(
+      Trajectory,
+      'new_traj',
+      10)
+    self._subscription = self.create_subscription(
+      SensorData,
+      'traj_start_data',
+      self.create_trajectory_callback,
+      10)
 
   def create_trajectory_callback(self, msg):
-    self.get_logger().info('Received: %s' % msg.data)
+    self.get_logger().info('Received: {}'.format(msg.data))
 
     reply = Trajectory()
     reply.data = 'This will contain new trajectory data'
