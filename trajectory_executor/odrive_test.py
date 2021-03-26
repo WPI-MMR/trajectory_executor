7433fa37c1124ec20b6be511a21e9245f79914c1
Andrew Euredjian <andrew_103@rocketmail.com>
2021-03-26 00:00:42 +0000

    First run of testing end to end pipeline

---
+++ trajectory_executor/odrive_test.py
@@ -0,0 +1,50 @@
+import rclpy
+from rclpy.node import Node
+
+from trajectory_interfaces.msg import JointAngles
+from trajectory_interfaces.srv import SensorDataRequest
+
+import serial
+import random
+
+
+class OdriveTest(Node):
+  """Tests sending a setpoint to the arduino/odrive and receiving tracking information"""
+  def __init__(self):
+    super().__init__('odrive_text')
+
+    self.publisher = self.create_publisher(
+      JointAngles,
+      'joint_angles',
+      10)
+
+    self.srv_client = self.create_client(
+      SensorDataRequest,
+      'sensor_data_request')
+
+
+def main(args=None):
+  rclpy.init(args=args)
+  odrive_tester = OdriveTest()
+
+  movements = [] # list of joint angles
+
+  angles = JointAngles()
+  angles.left_hip = random.randint(0, 359)
+  angles.left_knee = random.randint(0, 359)
+  angles.right_hip = random.randint(0, 359)
+  angles.right_knee = random.randint(0, 359)
+  angles.left_shoulder = random.randint(0, 359)
+  angles.left_elbow = random.randint(0, 359)
+  angles.right_shoulder = random.randint(0, 359)
+  angles.right_elbow = random.randint(0, 359)
+
+  odrive_tester.publish(angles)
+  rclpy.spinOnce(odrive_tester)
+
+  # while rclpy.ok():
+  #   rclpy.spinOnce(odrive_tester)
+
+
+if __name__ == "__main__":
+  main()
