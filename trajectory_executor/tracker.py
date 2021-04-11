fa337deb59b2e33497e129764d70d1cdd18f48af
Andrew Euredjian <andrew_103@rocketmail.com>
2021-04-11 17:24:47 +0000

    Temporary solution to failed position tracking bug

--- trajectory_executor/tracker.py
+++ trajectory_executor/tracker.py
@@ -5,6 +5,7 @@ from trajectory_interfaces.msg import JointAngles, Trajectory
 from trajectory_interfaces.srv import SensorDataRequest
 
 import serial
+import time
 
 
 class TrajectoryTracker(Node):
@@ -30,41 +31,111 @@ class TrajectoryTracker(Node):
       self.get_logger().info("Serial service inactive, waiting...")
 
     self.req = SensorDataRequest.Request()
+    self.req.requested = True
+    self.traj_available = False
 
   def send_data_request(self):
-    self.req.requested = True
+    # self.req.requested = True
     self.future = self.srv_client.call_async(self.req)
 
   def traj_callback(self, traj: Trajectory):
     """Receive trajectory from generator node"""
-    self.get_logger.info("Trajectory received. Executing...")
-    for ja in traj:
-      self.ja_pub.publish(ja)
-      goal = False
+    self.get_logger().info("Trajectory received. Executing...")
+    self.traj = traj
+    self.traj_available = True
+    # for ja in traj.trajectory:
+    #   self.ja_pub.publish(ja)
+    #   goal = False
 
-      while rclpy.ok():
-        self.send_data_request()
-        while rclpy.ok():
-          # rclpy.spin_once(self)
-          if self.future.done():
-            try:
-              response = self.future.result()
-            except Exception as e:
-              self.get_logger().warn(f"Service call failed {e}")
-            else:
-              if response.at_goal:
-                goal = True
-              break
+    #   while rclpy.ok():
+    #     self.get_logger().info("spin")
+    #     self.send_data_request()
+    #     while rclpy.ok():
+    #       self.get_logger().info("future")
+    #       rclpy.spin_once(self)
+    #       if self.future.done():
+    #         try:
+    #           self.get_logger().info("response")
+    #           response = self.future.result()
+    #         except Exception as e:
+    #           self.get_logger().warn(f"Service call failed {e}")
+    #         else:
+    #           if response.at_goal:
+    #             goal = True
+    #           break
+    #     if goal:
+    #       break
 
-        if goal:
-          break
+  # def spin(self):
+  #   while rclpy.ok():
+  #     rclpy.spin_once(self)
+  #     if self.traj_available:
+  #       self.traj_available = False
+  #       for ja in self.traj.trajectory:
+  #         self.ja_pub.publish(ja)
+  #         goal = False
+
+  #         while rclpy.ok():
+  #           self.send_data_request()
+  #           while rclpy.ok():
+  #             rclpy.spin_once(self)
+  #             if self.future.done():
+  #               try:
+  #                 response = self.future.done()
+  #               except Exception as e:
+  #                 self.get_logger().warn(f"Service call failed {e}")
+  #               else:
+  #                 if response.at_goal:
+  #                   goal = True
+  #                 break
+  #           if goal:
+  #             break
+
 
 
 def main(args=None):
   rclpy.init(args=args)
   trajectory_tracker = TrajectoryTracker()
 
-  rclpy.spin(trajectory_tracker)
+  trajectory_tracker.get_logger().info("Ready")
+  # rclpy.spin(trajectory_tracker)
+  # while rclpy.ok():
+  #   rclpy.spin_once(trajectory_tracker)
+
+  while rclpy.ok():
+    rclpy.spin_once(trajectory_tracker)
+    if trajectory_tracker.traj_available:
+      trajectory_tracker.traj_available = False
+      for ja in trajectory_tracker.traj.trajectory:
+        time.sleep(0.5)
+        trajectory_tracker.ja_pub.publish(ja)
+        # trajectory_tracker.get_logger().info(f"Goal: {ja.left_hip}")
+        goal = False
+
+        while rclpy.ok():
+          # response = trajectory_tracker.srv_client.call(trajectory_tracker.req)
+          # rclpy.spin_once(trajectory_tracker)
+          # if response.at_goal:
+          #   break
+          trajectory_tracker.send_data_request()
+          while rclpy.ok():
+            rclpy.spin_once(trajectory_tracker)
+            if trajectory_tracker.future.done():
+              try:
+                response = trajectory_tracker.future.result()
+              except Exception as e:
+                self.get_logger().warn(f"Service call failed {e}")
+              else:
+                # trajectory_tracker.get_logger().info(f"L_HIP: {response.l_hip}")
+                if response.at_goal:
+                  trajectory_tracker.future = None
+                  goal = True
+                break
+          if goal:
+            goal = False
+            break
+
+  # trajectory_tracker.spin()
   rclpy.shutdown()
 
 
--- trajectory_executor/traj_test.py
+++ trajectory_executor/traj_test.py
@@ -4,6 +4,7 @@ from rclpy.node import Node
 from trajectory_interfaces.msg import JointAngles, Trajectory
 
 import random
+import time
 
 class TrajTest(Node):
   """Tests sending a trajectory to the tracker node for execution"""
@@ -22,13 +23,33 @@ def main(args=None):
 
   num_ja = 10
   traj = Trajectory()
+  traj_tester.get_logger().info("Waiting 2 seconds")
+  time.sleep(2)
+
+  l_hip_angle = 0
+  l_knee_angle = 0
 
   # Generate joint angles and fill trajectory object
   for i in range(num_ja):
+    if i % 2 == 0:
+      l_hip_angle += 45
+      l_knee_angle -= 45
+      if l_hip_angle >= 360:
+        l_hip_angle -= 360
+      if l_knee_angle < 0:
+        l_knee_angle += 360
+    else:
+      l_hip_angle -= 90
+      l_knee_angle += 90
+      if l_hip_angle < 0:
+        l_hip_angle += 360      
+      if l_knee_angle >= 360:
+        l_knee_angle -= 360
+
     angles = JointAngles()
 
-    angles.left_hip = random.randint(0, 359)
-    angles.left_knee = random.randint(0, 359)
+    angles.left_hip = l_hip_angle
+    angles.left_knee = l_knee_angle
     # angles.right_hip = random.randint(0, 359)
     # angles.right_knee = random.randint(0, 359)
     # angles.left_shoulder = random.randint(0, 359)
@@ -46,6 +67,7 @@ def main(args=None):
 
   # Publish new trajectory to ros topic
   traj_tester.publisher.publish(traj)
+  # time.sleep(1)
 
   traj_tester.destroy_node()
   rclpy.shutdown()
