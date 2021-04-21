4168f229b4a46d804a43527be7c30420b31892bd
Ankur Gupta <rukna1000@gmail.com>
2021-04-20 20:39:41 -0400

    update interval rate for joints

--- trajectory_executor/algos/foward_wave.py
+++ trajectory_executor/algos/foward_wave.py
@@ -682,7 +682,7 @@ if __name__ == '__main__':
 
   """
   # sim.wave()
-  sim.generate_checkpoints(0.01)
+  sim.generate_checkpoints(0.0075)
   # sim.visualize_leg_movements()
   # fig, axes = plt.subplots()
   # line = sim.init_plot(axes, 'Leg')
